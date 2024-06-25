a369f292a97c55cd184ede94ff6297dc6486525e
Gurudarshan57 <venom573116@gmail.com>
2024-06-25 22:08:02 +0530

    implemented password hashing

--- Models/user.js
+++ Models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
+const { createHmac, randomBytes } = require("crypto")
 
-const Schema = new Mongoose.schema({
+const schema = new Schema({
     fullname: {
         type: String,
         required: true
@@ -9,12 +11,52 @@ const Schema = new Mongoose.schema({
         type: String,
         required: true,
         unique: true
-    }, password: {
+    }, salt: {
+        type: String
+    },
+    password: {
         type: String,
         required: true
     }
+    , profileImg: {
+        type: String,
+        default: '/images/default.png'
+    }
+    , role: {
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER"
+    }
+}, { timestamps: true })
+
+schema.pre('save', function (next) {
+    const user = this;
+    if (!user.isModified("password")) return;
+
+    const salt = randomBytes(16).toString();
+    const hashedpassword = createHmac("sha256", salt).update(user.password).digest("hex")
+
+    this.salt = salt
+    this.password = hashedpassword
+    next();
+});
+
+schema.static('matchPassword', async function (email, password) {
+    const user = await this.findOne({ email: email })
+    if (!user) return -1
+
+    const inputPassword = createHmac("sha256", user.salt).update(password).digest("hex")
+    const realPassword = user.password
+
+    return inputPassword === realPassword ? true : false
 })
 
-const user = Mongoose.model("users", Schema)
+const user = model('users', schema)
+
+// async function delAll() {
+//     await user.deleteMany({})
+// }
+
+// delAll()
 
 module.exports = user
--- Routes/user.js
+++ Routes/user.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const user = require("../Models/user")
 
 const Router = express.Router();
 
@@ -8,14 +9,19 @@ Router.route("/").get((req, res) => {
 
 Router.route("/signin").get((req, res) => [
     res.render("SignIn")
-]).post((req, res) => {
-    console.log(req.body)
+]).post(async (req, res) => {
+    const { email, password } = req.body
+    const data = await user.matchPassword(email, password)
+    res.json({ msg: data })
+
 })
 
 Router.route("/signup").get((req, res) => [
     res.render("SignUp")
-]).post((req, res) => {
-    console.log(req.body)
+]).post(async (req, res) => {
+    const { fullname, email, password } = req.body
+    await user.create({ fullname, email, password })
+    res.redirect("/signin")
 })
 
 module.exports = Router
